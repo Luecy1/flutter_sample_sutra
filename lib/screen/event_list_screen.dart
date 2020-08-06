1cb8fa2cf10ad627c7cc71a9dd644fb2936ef363
Youhei Oonuma <oonuma617@gmail.com>
2020-08-07 05:58:46 +0900

    add

--- lib/screen/event_list_screen.dart
+++ lib/screen/event_list_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_sample_sutra/blocs/event_list/event_list_bloc.dart';
 import 'package:flutter_sample_sutra/blocs/event_list/event_list_event.dart';
+import 'package:flutter_sample_sutra/blocs/event_list/event_list_state.dart';
 import 'package:flutter_sample_sutra/repository/hardcord_event_list_reopository.dart';
 
 class EventListScreen extends StatelessWidget {
@@ -10,6 +12,23 @@ class EventListScreen extends StatelessWidget {
       eventListRepository: HardcordEventListRepository(),
     );
     eventListBloc.dispatch(EventListLoad());
-    return Container();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Events'),
+      ),
+      body: BlocBuilder<EventListBloc, EventListState>(
+        bloc: eventListBloc,
+        builder: (context, state) {
+          if (state is EventListInProgress) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return Container();
+        },
+      ),
+    );
   }
 }
