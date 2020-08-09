a94f09e8533351f49959c5d4449a5e2dc11dff8e
Youhei Oonuma <oonuma617@gmail.com>
2020-08-09 10:43:45 +0900

    add auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_sample_sutra/blocs/authentication/authentication_bloc.dart';
+import 'package:flutter_sample_sutra/blocs/authentication/authentication_event.dart';
+import 'package:flutter_sample_sutra/repository/hardcode_authentication_repository.dart';
 import 'package:flutter_sample_sutra/screen/event_list_screen.dart';
+import 'package:flutter_sample_sutra/screen/splash_screen.dart';
+
+import 'blocs/authentication/authentication_state.dart';
 
 void main() {
-  runApp(MyApp());
+  final authenticationRepository = HardcodeAuthenticationRepository();
+
+  runApp(
+    BlocProvider<AuthenticationBloc>(
+      builder: (context) {
+        return AuthenticationBloc(authenticationRepository: authenticationRepository)
+          ..dispatch(AppStarted());
+      },
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final authBloc = BlocProvider.of<AuthenticationBloc>(context);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -15,7 +33,18 @@ class MyApp extends StatelessWidget {
         accentColor: Colors.pink[800],
         brightness: Brightness.light,
       ),
-      home: EventListScreen(),
+      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+        bloc: authBloc,
+        builder: (context, state) {
+          if (state is AuthenticationInProgress) {
+            return SplashScreen();
+          }
+          if (state is AuthenticationSuccess) {
+            return EventListScreen();
+          }
+          return Container();
+        },
+      ),
     );
   }
 }
